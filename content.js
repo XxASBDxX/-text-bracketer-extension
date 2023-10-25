01245302d9805ae8d4058df5328d7351880f8093
XxASBDxX <110325227+XxASBDxX@users.noreply.github.com>
2023-10-25 01:20:55 -0400

    content.js

--- content.js
+++ content.js
@@ -1,1 +1,11 @@
-c
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "wrapText") {
+    const brackets = request.brackets;
+    const selectedText = window.getSelection().toString();
+    const wrappedText = brackets[0] + selectedText + brackets[1];
+
+    // Replace the selected text with the wrapped text
+    document.execCommand("insertText", false, wrappedText);
+  }
+});
+
