7b873da6978794846011467918463a978cacd885
XxASBDxX <110325227+XxASBDxX@users.noreply.github.com>
2023-10-25 01:21:38 -0400

    popup.js

--- popup.js
+++ popup.js
@@ -1,1 +1,35 @@
+function wrapTextWithBrackets(brackets) {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.scripting.executeScript({
+      target: { tabId: tabs[0].id },
+      function: (brackets) => {
+        chrome.runtime.sendMessage({ action: "wrapText", brackets });
+      },
+      args: [brackets],
+    });
+  });
+}
 
+document.getElementById("wrapDoubleQuotes").addEventListener("click", () => {
+  wrapTextWithBrackets(['"', '"']);
+});
+
+document.getElementById("wrapSingleQuotes").addEventListener("click", () => {
+  wrapTextWithBrackets(["'", "'"]);
+});
+
+document.getElementById("wrapParentheses").addEventListener("click", () => {
+  wrapTextWithBrackets(["(", ")"]);
+});
+
+document.getElementById("wrapBrackets").addEventListener("click", () => {
+  wrapTextWithBrackets(["[", "]"]);
+});
+
+document.getElementById("wrapCurlyBraces").addEventListener("click", () => {
+  wrapTextWithBrackets(["{", "}"]);
+});
+
+document.getElementById("wrapAngleBrackets").addEventListener("click", () => {
+  wrapTextWithBrackets(["<", ">"]);
+});
